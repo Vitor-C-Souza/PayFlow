118cd32d3db93dc741342d08585d32ba33384c68
Vitor-C-Souza <vitor_souza97@hotmail.com>
2026-02-24 12:56:26 -0300

    feat: dockerize services and add docker-compose infrastructure
    - Added Dockerfile for wallet-service and transaction-service
    - Created docker-compose with MySQL and RabbitMQ
    - Configured services to use environment variables in prod profile
    - Added healthcheck for MySQL and RabbitMQ
    - Enabled container restart policy
    - Configured network and volumes for persistence

--- transaction-service/src/main/java/br/me/vitorcsouza/payflow/transaction/consumer/TransactionConsumer.java
+++
@@ -1,4 +0,0 @@
-package br.me.vitorcsouza.payflow.transaction.consumer;
-
-public class TransactionConsumer {
-}
--- transaction-service/src/main/java/br/me/vitorcsouza/payflow/transaction/infra/messaging/TransactionConsumer.java
+++ transaction-service/src/main/java/br/me/vitorcsouza/payflow/transaction/infra/messaging/TransactionConsumer.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
